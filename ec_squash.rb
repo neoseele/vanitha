93655edbee5c1a45c10d950a52ec71dd9d7d67b4
Neil Miao <nmiao@redhat.com>
2014-02-03 15:33:58 +1000

    updated ec_squash.rb

--- ec_squash.rb
+++ ec_squash.rb
@@ -55,7 +55,7 @@ class Entry
 end
 
 class Qa
-  attr_accessor :q, :a, :order, :start_time, :end_time
+  attr_accessor :q, :a, :order, :start_time, :end_time, :is_last
 
   def initialize(q, order=1, a={})
     @q = q
@@ -95,68 +95,83 @@ output = File.join(output_dir,output_file)
 rows = CSV.read(input, {:headers => :false})
 
 qas = []
-last_qa = nil
+current_qa = nil
 order = 1
 
 rows.each do |r|
   e = Entry.new(r)
 
-  # save the first entry into last_qa
-  if last_qa.nil?
+  # save the first entry into current_qa
+  if current_qa.nil?
     if e.ca == 'A'
-      last_qa = Qa.new(e,order)
-      qas << last_qa
+      current_qa = Qa.new(e,order)
+      qas << current_qa
     end
     next
   end
 
-  last_q = last_qa.q
+  q = current_qa.q
 
-  # current entry is a A
+  # current entry is from a A
   if e.ca == 'A'
-    # same ticker and same full_name ? => merge them
-    if e =~ last_q
-      last_q.merge e
+    if e =~ q
+      # same ticker and same full_name ? => merge them
+      q.merge e
+
       # increase the qa round's end_time
-      last_qa.update_end_time e
+      current_qa.update_end_time e
+
+      # move onto the next row
       next
     end
 
-    # from this point, either ticker or full_name changed
+    # from this point, either ticker or e's full_name changed
+    # either way the current Q&A round is finished
 
-    # ticker is the same ? increase order : reset order
-    (e.ticker == last_q.ticker) ? order += 1 : order = 1
-
-    # create a new qa object
-    last_qa  = Qa.new(e,order)
-    qas << last_qa
-    next
-  end
-
-  # current entry is a C
-  if e.ticker == last_q.ticker
-    last_a = last_qa.a
-
-    if last_a.key?(e.full_name)
-      # a existing C found in answer hash => merge them 
-      last_a[e.full_name].merge e
+    # increase order if the ticker un-changed
+    if e.ticker == q.ticker
+      order += 1
     else
-      # new C found, add it into the answer hash
-      last_qa.a[e.full_name] = e
+      # reset order to 1, flag the ticker's last Q&A
+      order = 1
+      current_qa.is_last = true
     end
 
-    # increase the qa round's end_time
-    last_qa.update_end_time e
-  end
+    # create a new qa object
+    current_qa  = Qa.new(e,order)
+    qas << current_qa
 
+  # current entry is from a C
+  else
+
+    # ticker should always matched
+    if e.ticker == q.ticker
+      a = current_qa.a
+
+      if a.key?(e.full_name)
+        # an existing C found in answer hash => merge them
+        a[e.full_name].merge e
+      else
+        # new C found, add it into the answer hash
+        current_qa.a[e.full_name] = e
+      end
+
+      # increase the qa round's end_time
+      current_qa.update_end_time e
+    end
+
+  end
 end
+
+# flag the dead last Q&A
+qas[-1].is_last = true
 
 #pp qas.length
 
-@csv = [['ticker','date','reason','ca','first_nm','surname','affln','firm','jobt','no_words','no_questions','no_words_having_questions','call_date','no_people_respond','order','start_time','end_time']]
+@csv = [['ticker','date','reason','ca','first_nm','surname','affln','firm','jobt','no_words','no_questions','no_words_having_questions','call_date','no_people_respond','order','start_time','end_time','last_one?']]
 
 qas.each do |qa|
-  @csv << qa.q.to_a + [qa.a.length,qa.order,qa.start_time.to_str,qa.end_time.to_str]
+  @csv << qa.q.to_a + [qa.a.length,qa.order,qa.start_time.to_str,qa.end_time.to_str,qa.is_last]
   qa.a.values.each do |e|
     @csv << e.to_a
   end
