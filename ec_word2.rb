ac3ba8b71868e4943548e18a76f05fda06d9a805
Neil Miao <nmiao@redhat.com>
2014-11-07 19:08:55 +1000

    ec_word2.rb updated

--- ec_word2.rb
+++ ec_word2.rb
@@ -5,6 +5,7 @@ require 'time'
 
 # transcript look up table
 $transcripts = {}
+$word_count = {}
 
 class Transcript
   attr_accessor :reason, :speeches
@@ -31,38 +32,44 @@ class Transcript
     found
   end
 
-  def self.merge_word_frequences(speeches)
-    word_frequence = {}
+  def self.merge_word_count(speeches, word_count = {})
     while speeches.size > 0
-      word_frequence.merge!(speeches.shift.word_frequence) { |k,old,new| old + new }
+      word_count.merge!(speeches.shift.word_count) { |k,old,new| old + new }
     end
-    word_frequence
+    word_count
+  end
+
+  def self.merge_words(speeches, words = '')
+    speeches.each do |s|
+      words += s.text.gsub(/\r\n?/,'').gsub(/\n/,'')
+    end
+    words.strip
   end
 end
 
 class Speech
-  attr_accessor :speaker, :start_time, :end_time, :text, :word_frequence
+  attr_accessor :speaker, :start_time, :end_time, :text, :word_count
   def initialize r
     @speaker = r['Transcript Speaker'].downcase
     @start_time = Time.parse(r['Start Time'].split(' ')[1])
     @end_time = Time.parse(r['End Time'].split(' ')[1])
     @text = r['Transcript Text'].downcase
-    cal_word_frequence
+    cal_word_count
   end
 
   private
 
-  def cal_word_frequence
-    @word_frequence = {}
+  def cal_word_count
+    @word_count = {}
     @text.split(' ').each do |w|
       word = w.downcase.gsub(/\W/, '')
       next unless word != ''
 
-      if @word_frequence[word].nil?
-        @word_frequence[word] = 1
+      if @word_count[word].nil?
+        @word_count[word] = 1
         next
       end
-      @word_frequence[word] += 1
+      @word_count[word] += 1
     end
   end
 end
@@ -88,11 +95,10 @@ class Worker < Base
   def run
     src = @options.source
     dir = File.dirname src
-
     jump = read_csv(src)
 
     # create the output array
-    out = [jump.headers + ['count_by_speaker', 'count_by_time']]
+    out = [jump.headers + ['Words Spoken']]
 
     # loop through each row in jump file
     jump.each do |r|
@@ -123,16 +129,27 @@ class Worker < Base
           end
         end
 
+        # add the transcript to the lookup table
         $transcripts[reason] = t
       end
 
-      ss = t.find_speech_by_speaker j.full_name
+      #ss = t.find_speech_by_speaker j.full_name
       st = t.find_speech_by_time j.start_talk_time, j.end_talk_time
 
-      out << r.to_hash.merge({'count_by_speaker' => Transcript.merge_word_frequences(ss), 'count_by_time' => Transcript.merge_word_frequences(st)}).values
-
+      out << r.to_hash.merge({'words_spoken' => Transcript.merge_words(st)}).values
+      Transcript.merge_word_count(st, $word_count)
     end
+
+    # write the updated jump file
     csv_out File.join(dir,'out.csv'), out
+
+    out_count = [['word','count']]
+    $word_count.each do |word,count|
+      out_count << [word, count]
+    end
+
+    # write the word count
+    csv_out File.join(dir, 'out_count.csv'), out_count
   end
 end
 
